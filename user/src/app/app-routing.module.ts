b6ab0893d7163b3519b52aa5130f8b80f46b2286
ozgucdlg <ozgucdalga@gmail.com>
2023-07-04 09:57:38 -0400

    created routings and completed category component

--- user/src/app/app-routing.module.ts
+++ user/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from './components/user/user.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"user",component:UserComponent},
+  {path:"", component:DashboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
